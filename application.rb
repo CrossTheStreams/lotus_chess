dcb98211052b77be0a3b93dc8cade777fb41fa16
Andrew <arhautau@gmail.com>
2014-08-02 23:16:45 -0700

    jasmine for JS testing

--- application.rb
+++ application.rb
@@ -1,14 +1,22 @@
 require 'lotus'
 
 module WebChess
+
+  def self.env
+    @@env 
+  end
+
   class Application < Lotus::Application
     configure do
       routes 'config/routes'
+
+      WebChess.class_variable_set(:@@env,ENV['RACK_ENV'])
 
       load_paths << [
         'app/controllers',
         'app/views'
       ]
     end
+
   end
 end
